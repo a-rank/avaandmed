f1a5b2b46f8167b8bed360d5e777d0fa531f2273
rank <allan@rank.ee>
2016-08-02 18:09:34 +0300

    reshuffling

--- database.py
+++ database.py
@@ -18,12 +18,12 @@ from flask_script import Manager
 from app.context import Doku
 from pprint import pprint
 from itertools import islice, takewhile
-from collections import OrderedDict
 
 manager = Manager(usage="Perform database operations", description="")
 
 
-def downloaded_callback(files):
+def downloaded_callback(id, files):
+    print id
     pprint(files)
 
 
@@ -34,11 +34,6 @@ def fetch():
     doku = Doku(amphora_location=app.config["AMPHORA_LOCATION"],
                 temp_dir=app.config["TEMP_DIR"])
 
-    documents = doku.download_file_list(app.config["AMPHORA_TOPICS"])
-    current_index = documents.keys().index("269660")
-    download = dict(islice(documents.items(), 0, current_index))
-
-    downloaded = doku.download_files(file_list=download, delay=0, extract_text=True,
-                                     callback=downloaded_callback)
-
-    print len(documents)
+    downloaded = doku.download_documents(topic_filter=app.config["AMPHORA_TOPICS"], id_stop_at=269660,
+                                         delay=1, extract_text=True, callback=downloaded_callback)
+    print len(downloaded)
--- modules/doku/doku.py
+++ modules/doku/doku.py
@@ -20,6 +20,7 @@ from collections import defaultdict, OrderedDict
 from operator import setitem
 from time import sleep
 from cgi import parse_header
+from itertools import islice
 
 
 class DownloadError(Exception):
@@ -42,74 +43,92 @@ class Doku(object):
         else:
             self.temp_dir = temp_dir
 
-    def create_file_url(self, item_id, item_file_id):
+    def create_document_url(self, item_id, item_file_id):
         return "{amphora_url}/?itm={itm}&af={af}".format(amphora_url=self.amphora_url,
                                                          itm=item_id, af=item_file_id)
 
-    def extract_text_to_file(self, filename, filename_txt=None):
-        if not filename_txt:
-            filename_txt = "".join([filename, ".txt"])
+    def extract_document_text(self, filename, out_filename=None):
+        if not out_filename:
+            out_filename = "".join([filename, ".txt"])
         text = fulltext.get(filename)
-        with open(filename_txt, "w") as f:
+        with open(out_filename, "w") as f:
             f.write(text)
-        return filename_txt
+        return out_filename
 
-    def download_file(self, url, filename, block_size=1024, timeout=20, extension_from_header=False):
+    def download_file(self, url, out_filename, block_size=1024,
+                      timeout=20, extension_from_header=False):
         response = requests.get(url, stream=True, timeout=timeout)
         if response.ok:
             if extension_from_header:
                 _, params = parse_header(response.headers.get("content-disposition", ""))
-                _, extension = os_path.splitext(params["filename"])
+                _, extension = os_path.splitext(params.get("filename", ""))
                 if not extension:
                     extension = ".doc"
-                filename = "".join([filename, extension])
-            with open(filename, "w") as f:
+                out_filename = "".join([out_filename, extension])
+            with open(out_filename, "w") as f:
                 for block in response.iter_content(block_size):
                     f.write(block)
-            return filename
+            return out_filename
         else:
             raise DownloadError(response.status_code, url)
 
-    def download_file_list(self, filter_topic_ids):
+    def download_documents_list(self, topic_filter):
         filepath = os_path.join(self.temp_dir, "act.json")
         self.download_file(self.amphora_api_url, filepath)
 
         files = OrderedDict()
-        ids = defaultdict(int)
+        attributes = defaultdict(int)
         setters = {
-            "Acts.item.topic_id.number": lambda ids, value: setitem(ids, "topic_id", value),
-            "Acts.item.item_id.number": lambda ids, value: setitem(ids, "item_id", value),
-            "Acts.item.item_file_id.number": lambda ids, value: setitem(ids, "item_file_id", value),
-            "Acts.item.end_map": lambda ids, value: ids.clear()
+            "Acts.item.topic_id.number": lambda attributes, value: setitem(attributes, "topic_id", value),
+            "Acts.item.item_id.number": lambda attributes, value: setitem(attributes, "item_id", value),
+            "Acts.item.item_file_id.number": lambda attributes, value: setitem(attributes, "file_id", value),
+            "Acts.item.public_title.string": lambda attributes, value: setitem(attributes, "title", value),
+            "Acts.item.object_date.string": lambda attributes, value: setitem(attributes, "date", value),
+            "Acts.item.end_map": lambda attributes, value: attributes.clear()
         }
 
         with open(filepath, "r") as f:
             parser = ijson_parse(f)
             for prefix, event, value in parser:
                 if (prefix, event) == ("Acts.item", "end_map"):
-                    if ids["topic_id"] in filter_topic_ids and ids["item_id"] and ids["item_file_id"]:
-                        files[str(ids["item_id"])] = self.create_file_url(ids["item_id"], ids["item_file_id"])
+                    if attributes["topic_id"] in topic_filter and attributes["item_id"] and attributes["file_id"]:
+                        files[attributes["item_id"]] = (attributes["file_id"],
+                                                        attributes["topic_id"],
+                                                        attributes["title"],
+                                                        attributes["date"])
 
                 setter = setters.get("".join([prefix, ".", event]))
                 if setter:
-                    setter(ids, value)
-
+                    setter(attributes, value)
         return files
 
-    def download_files(self, file_list, delay=None, extract_text=False, callback=None):
+    def download_documents(self, topic_filter, id_stop_at=None,
+                           delay=None, extract_text=False, callback=None):
         downloaded_files = {}
-        for item_id, url in file_list.items():
-            filename = os_path.join(self.temp_dir, item_id)
-            downloaded_file = self.download_file(url, filename, extension_from_header=True)
-            downloaded_files[item_id] = {"file": downloaded_file}
+        documents = self.download_documents_list(topic_filter)
+        if len(documents):
+            id_start_at = documents.keys()[0]
 
-            if extract_text:
-                downloaded_files[item_id]["text"] = self.extract_text_to_file(downloaded_file)
+            if id_stop_at:
+                stop_at = documents.keys().index(id_stop_at)
+                documents = OrderedDict(islice(documents.items(), 0, stop_at))
 
-            if callback:
-                callback(downloaded_files[item_id])
+            for item_id, data in documents.items():
+                file_id, _, _, _ = data
+                url = self.create_document_url(item_id, file_id)
+                filename = os_path.join(self.temp_dir, str(item_id))
 
-            if delay:
-                sleep(delay)
+                downloaded_file = self.download_file(url, filename, extension_from_header=True)
+                downloaded_files[item_id] = {"file": downloaded_file,
+                                             "data": data}
+
+                if extract_text:
+                    downloaded_files[item_id]["text"] = self.extract_document_text(downloaded_file)
+
+                if callback:
+                    callback(item_id, downloaded_files[item_id])
+
+                if delay:
+                    sleep(delay)
 
         return downloaded_files
